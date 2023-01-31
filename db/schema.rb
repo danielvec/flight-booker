b0131148089d7a6ac48ab057d20239c79019bf02
danielvec <vechioladaniel@gmail.com>
2023-01-30 16:49:28 -0800

    add flight association to airport model and add flights to seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_30_042521) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_30_061540) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.datetime "time"
+    t.integer "duration"
+    t.integer "origin_id"
+    t.integer "destination_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Airport.destroy_all
+Airport.delete_all
+Flight.delete_all
 
 Airport.create!(
   [
@@ -15,6 +16,15 @@ Airport.create!(
     {code: "PHX"},
     {code: "PDX"},
     {code: "DTW"}
+  ]
+)
+
+Flight.create!(
+  [
+    {time: DateTime.now, duration: 60, origin_id: 1, destination_id: 2},
+    {time: DateTime.now, duration: 60, origin_id: 1, destination_id: 3},
+    {time: DateTime.now, duration: 90, origin_id: 1, destination_id: 4},
+    {time: DateTime.now, duration: 300, origin_id: 1, destination_id: 5},
   ]
 )
 
