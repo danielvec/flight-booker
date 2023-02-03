4360afb698db0b8327fc19540b9b9e6ee8324514
danielvec <vechioladaniel@gmail.com>
2023-02-02 19:31:31 -0600

    set up querying methods to pull matching flights from database

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,8 @@ class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map{ |a| [ a.code, a.id ]}
     @date_options = Flight.all.map{ |d| [ d.time.strftime("%m/%d/%Y") ]}.uniq
+    @flights = Flight.all.matching_flights(params[:origin_id], params[:destination_id])
+    @flights = @flights.same_date(params[:date]) if params[:date] 
+    console
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :origin, class_name: "Airport"
   belongs_to :destination, class_name: "Airport"
+
+  scope :matching_flights, ->(origin, destination){ where(origin_id: origin).where(destination_id: destination)}
+  scope :same_date, -> (date){ where('time <= ?', DateTime.parse(date).end_of_day).where('time >= ?', DateTime.parse(date).beginning_of_day) }
 end
