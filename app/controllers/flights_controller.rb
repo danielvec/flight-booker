387e19e1ee7dffb62e33c44a918ed8983ce09583
danielvec <vechioladaniel@gmail.com>
2023-02-04 00:02:41 -0800

    sort date options

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,9 +1,9 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map{ |a| [ a.code, a.id ]}
-    @date_options = Flight.all.map{ |d| [ d.time.strftime("%m/%d/%Y") ]}.uniq
+    @date_options = Flight.all.map{ |d| [ d.time.strftime("%m/%d/%Y") ]}.uniq.sort
     @flights = Flight.all.matching_flights(params[:origin_id], params[:destination_id])
-    @flights = @flights.same_date(params[:date]) if params[:date] 
+    @flights = @flights.same_date(params[:date]) if params[:date]
     console
   end
 end
