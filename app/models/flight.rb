d3e0903870d72475c799f9ff2a6454c2f4dcc1e8
danielvec <vechioladaniel@gmail.com>
2023-02-04 10:29:48 -0800

    update same_date scope to use Date instead of DateTime

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,5 +3,5 @@ class Flight < ApplicationRecord
   belongs_to :destination, class_name: "Airport"
 
   scope :matching_flights, ->(origin, destination){ where(origin_id: origin).where(destination_id: destination)}
-  scope :same_date, -> (date){ where('time <= ?', DateTime.parse(date).end_of_day).where('time >= ?', DateTime.parse(date).beginning_of_day) }
+  scope :same_date, -> (date){ where('time <= ?', Date.strptime(date, "%m/%d/%Y").end_of_day).where('time >= ?', Date.strptime(date, "%m/%d/%Y").beginning_of_day) }
 end
