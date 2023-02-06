19ff1429bb0eefd8af2e7f5631de9199e487dbd4
danielvec <vechioladaniel@gmail.com>
2023-02-06 10:34:15 -0800

    add background image to css and update index page style

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,6 +4,5 @@ class FlightsController < ApplicationController
     @date_options = Flight.all.map{ |d| [ d.time.strftime("%m/%d/%Y") ]}.uniq.sort
     @flights = Flight.all.matching_flights(params[:origin_id], params[:destination_id])
     @flights = @flights.same_date(params[:date]) if params[:date]
-    console
   end
 end
