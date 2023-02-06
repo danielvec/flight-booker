2cebe048a266bc665e26aa06b36acd5a880f0220
danielvec <vechioladaniel@gmail.com>
2023-02-06 10:37:01 -0800

    create bookings and passengers and set up relationships

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :origin, class_name: "Airport"
   belongs_to :destination, class_name: "Airport"
+  has_many :bookings
 
   scope :matching_flights, ->(origin, destination){ where(origin_id: origin).where(destination_id: destination)}
   scope :same_date, -> (date){ where('time <= ?', Date.strptime(date, "%m/%d/%Y").end_of_day).where('time >= ?', Date.strptime(date, "%m/%d/%Y").beginning_of_day) }
