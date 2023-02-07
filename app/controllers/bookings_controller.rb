eca08c99e662cd6d0c559c5befdccfa28012b1cf
danielvec <vechioladaniel@gmail.com>
2023-02-06 21:28:52 -0800

    display flight information on booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,9 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @flight = Flight.find(params[:flight])
+    @origin = Airport.find(@flight.origin_id)
+    @destination = Airport.find(@flight.destination_id)
+    console
   end
 end
