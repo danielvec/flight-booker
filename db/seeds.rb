5a65b6ccb951a82db7806ba5fde44026f1f98109
danielvec <vechioladaniel@gmail.com>
2023-02-08 22:36:47 -0800

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,26 +21,26 @@ Airport.create!(
 
 18.times do Flight.create!(
   [
-    {time: DateTime.now.beginning_of_day + 1 + rand(0.0..4.0), duration: 60, origin_id: 1, destination_id: 2},
-    {time: DateTime.now.beginning_of_day + 2 + rand(0.0..4.0), duration: 180, origin_id: 1, destination_id: 3},
-    {time: DateTime.now.beginning_of_day + 3 + rand(0.0..4.0), duration: 140, origin_id: 1, destination_id: 4},
-    {time: DateTime.now.beginning_of_day + 4 + rand(0.0..4.0), duration: 60, origin_id: 1, destination_id: 5},
-    {time: DateTime.now.beginning_of_day + 1 + rand(0.0..4.0), duration: 60, origin_id: 2, destination_id: 1},
-    {time: DateTime.now.beginning_of_day + 2 + rand(0.0..4.0), duration: 90, origin_id: 2, destination_id: 3},
-    {time: DateTime.now.beginning_of_day + 3 + rand(0.0..4.0), duration: 60, origin_id: 2, destination_id: 4},
-    {time: DateTime.now.beginning_of_day + 4 + rand(0.0..4.0), duration: 90, origin_id: 2, destination_id: 5},
-    {time: DateTime.now.beginning_of_day + 1 + rand(0.0..4.0), duration: 180, origin_id: 3, destination_id: 1},
-    {time: DateTime.now.beginning_of_day + 2 + rand(0.0..4.0), duration: 90, origin_id: 3, destination_id: 2},
-    {time: DateTime.now.beginning_of_day + 3 + rand(0.0..4.0), duration: 60, origin_id: 3, destination_id: 4},
-    {time: DateTime.now.beginning_of_day + 4 + rand(0.0..4.0), duration: 200, origin_id: 3, destination_id: 5},
-    {time: DateTime.now.beginning_of_day + 1 + rand(0.0..4.0), duration: 140, origin_id: 4, destination_id: 1},
-    {time: DateTime.now.beginning_of_day + 2 + rand(0.0..4.0), duration: 60, origin_id: 4, destination_id: 2},
-    {time: DateTime.now.beginning_of_day + 3 + rand(0.0..4.0), duration: 60, origin_id: 4, destination_id: 3},
-    {time: DateTime.now.beginning_of_day + 4 + rand(0.0..4.0), duration: 160, origin_id: 4, destination_id: 5},
-    {time: DateTime.now.beginning_of_day + 1 + rand(0.0..4.0), duration: 60, origin_id: 5, destination_id: 1},
-    {time: DateTime.now.beginning_of_day + 2 + rand(0.0..4.0), duration: 90, origin_id: 5, destination_id: 2},
-    {time: DateTime.now.beginning_of_day + 3 + rand(0.0..4.0), duration: 200, origin_id: 5, destination_id: 3},
-    {time: DateTime.now.beginning_of_day + 4 + rand(0.0..4.0), duration: 160, origin_id: 5, destination_id: 4}
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 60, origin_id: 1, destination_id: 2},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 180, origin_id: 1, destination_id: 3},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 140, origin_id: 1, destination_id: 4},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 60, origin_id: 1, destination_id: 5},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 60, origin_id: 2, destination_id: 1},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 90, origin_id: 2, destination_id: 3},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 60, origin_id: 2, destination_id: 4},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 90, origin_id: 2, destination_id: 5},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 180, origin_id: 3, destination_id: 1},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 90, origin_id: 3, destination_id: 2},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 60, origin_id: 3, destination_id: 4},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 200, origin_id: 3, destination_id: 5},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 140, origin_id: 4, destination_id: 1},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 60, origin_id: 4, destination_id: 2},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 60, origin_id: 4, destination_id: 3},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 160, origin_id: 4, destination_id: 5},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 60, origin_id: 5, destination_id: 1},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 90, origin_id: 5, destination_id: 2},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 200, origin_id: 5, destination_id: 3},
+    {time: DateTime.now.beginning_of_day + rand(1.0..8.0), duration: 160, origin_id: 5, destination_id: 4}
   ]
 )
   p "Created #{Airport.count} Airports"
