d55c07073ea2fb08980db7078eeac38548a35f9b
danielvec <vechioladaniel@gmail.com>
2023-02-09 22:01:29 -0800

    add routes for create and show for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   root "flights#index"
 
   resources :flights, only: [:index]
-  resources :bookings, only: [:new]
+  resources :bookings, only: [:new, :create, :show]
 end
