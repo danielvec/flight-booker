ce5c4768c9bdaf2b28db583650a21bae625cab2d
danielvec <vechioladaniel@gmail.com>
2023-02-11 09:21:49 -0800

    add form for adding passengers to a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,6 +4,24 @@ class BookingsController < ApplicationController
     @flight = Flight.find(params[:flight])
     @origin = Airport.find(@flight.origin_id)
     @destination = Airport.find(@flight.destination_id)
-    console
+    @passengers = (params[:passengers]).to_i
+    @passengers.times { @booking.passengers.build }
+  end
+
+  def create
+    @flight = Flight.find(params[:booking][:flight])
+    @booking = @flight.bookings.build(booking_params)
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:id, :first_name, :last_name, :email])
   end
 end
