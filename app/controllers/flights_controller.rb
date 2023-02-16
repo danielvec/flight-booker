013c53d5c28ceee6ad6feabf72f6eb15c0c9e148
danielvec <vechioladaniel@gmail.com>
2023-02-15 22:15:44 -0800

    add flash notice for no flights found

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,5 +4,9 @@ class FlightsController < ApplicationController
     @date_options = Flight.all.map{ |d| [ d.time.strftime("%m/%d/%Y") ]}.uniq.sort
     @flights = Flight.all.matching_flights(params[:origin_id], params[:destination_id])
     @flights = @flights.same_date(params[:date]) if params[:date]
+    @flights = @flights.sort_by {|f| f.price }
+    if @flights.empty?
+      flash.now[:notice] = "No flights found." if params[:searching]
+    end
   end
 end
