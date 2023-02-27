873c1922cfd286ac578f33e70835ea1ded355d22
danielvec <vechioladaniel@gmail.com>
2023-02-26 16:42:43 -0800

    create airports and flights in spec before running tests

--- spec/system/find_flight_spec.rb
+++ spec/system/find_flight_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Finding a flight', type: :system do
+  before do 
+    Airport.create!([ {code: "LAX", id: 1}, {code: "SFO", id:2}] )
+    5.times do Flight.create!( {time: DateTime.current.beginning_of_day + rand(1.0..2.0), 
+                                 duration: 60, 
+                                 origin_id: 1, 
+                                 destination_id: 2, 
+                                 price: rand(150..250)} )
+    end
+  end
   scenario 'valid inputs' do
     visit root_path
     click_on 'Find Flights'
